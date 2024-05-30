7e46985539cbe9069745f8ff97600503055973a2
Pablo Orviz <orviz@ifca.unican.es>
2024-05-30 14:40:05 +0200

    Add JePL file structure

---
+++ .sqa/script.gloppy-chocolate-sheepdog.sh
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/JanPremus/fd3d_TSN &&
+    git rev-parse --is-inside-work-tree
+)
